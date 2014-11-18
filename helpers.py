3c35ecda0e95d12b3ebad4f7de4d6b97b18ad991
gwongz <gwongz@gmail.com>
2014-11-17 20:51:54 -0800

    initial commit

---
+++ helpers.py
@@ -0,0 +1,39 @@
+import requests 
+from bs4 import BeautifulSoup
+
+index_to_day_names_map = {
+	0: 'Monday',
+	1: 'Tuesday',
+	2: 'Wednesday',
+	3: 'Thursday',
+	4: 'Friday',
+	5: 'Saturday',
+	6: 'Sunday'
+}
+
+def fmt_location(latitude, longitude):
+	""" Format a location string to 'latitude, longitude' format for GoogleAPI calls """
+	location = str(latitude) + ',' + str(longitude)
+	return location
+
+def fmt_where_constraint(latitude, longitude, radius):
+	""" Format the where query parameter for Soda API """
+	where = ['within_circle(location,', str(latitude), ',', str(longitude), ',', str(radius), ')']
+	return ''.join(where)
+
+def clean_link(link):
+	""" Scrape Yelp page for business website or return website from Google Places API """
+	if 'yelp' in link:
+		print 'parsing yelp html'
+		response = requests.get(link)
+		content = response.content
+		soup = BeautifulSoup(content)
+		div = soup.findAll('div',{'class':'biz-website'})
+		if len(div) > 0:
+		    link = div[0].find('a').text
+		    # should use better regex, link could start with www
+		    if not link.startswith('http'):
+		        link = 'http://www.' + link
+	return link 
+
+
